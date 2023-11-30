c5e530808b886e7af8ce742f967ce634b105ca81
rafi-ruetcse17 <rafialam6610@gmail.com>
2023-11-30 18:11:53 +0600

    second commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,12 +2,13 @@ const User = require('../models/userModel')
 const jwt = require('jsonwebtoken')
 
 const createToken = (_id) => {
-  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
+  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '9d' })
 }
 
 // login a user
 const loginUser = async (req, res) => {
   const {email, password} = req.body
+  console.log(req.body);
 
   try {
     const user = await User.login(email, password)
